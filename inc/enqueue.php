d7cd1da402cdb67fbf4e96193a156102039c0f6b
Aljosa Rencof <arencof@gmai.com>
2024-11-13 09:42:39 +0100

    fix animations and added icon to btn

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,7 +16,7 @@ function aquaar_script_enqueue() {
     wp_enqueue_style('Montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'); // Font Family
     wp_enqueue_style('Secondary', 'https://fonts.googleapis.com/css2?family=Anton&display=swap'); // Font Family
     //wp_enqueue_style('Slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css', array(), '1.0.0.', 'all');
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/main.min.css', array(), '1.0.1.', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/main.min.css', array(), '1.0.2.', 'all');
 
     // script
     wp_deregister_script('jquery');
--- template-parts/sections/projects-item.php
+++ template-parts/sections/projects-item.php
@@ -19,7 +19,10 @@
                 <div class="post-desc-skills">
                     <?php echo wp_kses_post(get_field('skills_and_deliverables')); ?>
                 </div>-->
-                <a href="<?php echo esc_html(get_field('web_link')); ?>" target="_blank" class="btn-link">Visit website</a>
+                <a href="<?php echo esc_html(get_field('web_link')); ?>" target="_blank" class="btn-link">
+                    Visit website
+                    <svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13 5h6v6h-1V6.707L6.104 18.604l-.707-.707L17.293 6H13z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
+                </a>
             </div>
         </div>
     </div>
