72cf378b3104179327f6a7fb6fddbd0cf1053ab0
Aljosa Rencof <aljosarencof@Mac.yallo.box>
2025-05-03 21:35:55 +0200

    fixes 2

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -15,7 +15,7 @@ function aquaar_script_enqueue() {
 
     wp_enqueue_style('Montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap'); // Font Family
     wp_enqueue_style('Secondary', 'https://fonts.googleapis.com/css2?family=Anton&display=swap'); // Font Family
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/main.min.css', array(), '1.2.1.', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/main.min.css', array(), '1.2.2.', 'all');
 
     // script
     wp_deregister_script('jquery');
